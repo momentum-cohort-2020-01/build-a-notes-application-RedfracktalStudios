0e1dc9c512e6d3a3d5a82b37c0d8ca2fb29f5bde
Marsha <marshakshearin@gmail.com>
2020-02-07 18:00:27 -0500

    effort

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('hello');
+
+document.getElementById('btnSave').addEventListener('click', btnSave);
+
+function btnSave() {
+  .then((result) => {
+
+}).catch((err) => {
+
+});
+
+
+  document.getElementById('btnWrite').addEventListener('click', btnWrite);
+
+  function btnWrite() {
+  .then((result) => {
+
+  }).catch((err) => {
+
+  });
